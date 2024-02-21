8e435725e3bfc9644462dde326752426205d3abc
gunitkalra <gunit92000@gmail.com>
2024-02-21 18:44:21 +0530

    auth change

--- server/controllers/auth.controler.js
+++ server/controllers/auth.controler.js
@@ -1,6 +1,6 @@
 import User from '../models/user.models.js'
 import bcryptjs from 'bcryptjs'
-export const signup=async(req,res)=>{
+export const signup=async(req,res,next)=>{
 const {username,email,password}= req.body
 if (!username || !email|| !password||username===''||email===''||password===''){
     return res.status(400).json({message:'all feilds are required'})
