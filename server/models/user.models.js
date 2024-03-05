57f6d1bdf5a0bdac863915f2de76f18c97423010
gunitkalra <gunit92000@gmail.com>
2024-03-05 18:52:29 +0530

    profile picture in dashboard

--- server/models/user.models.js
+++ server/models/user.models.js
@@ -16,6 +16,10 @@ const userSchema=new mongoose.Schema({
         type:String,
         required:true,
     },
+    profilePicture:{
+        type:String,
+        default: 'https://www.google.com/https://images.pexels.com/photos/214574/pexels-photo-214574.jpeg?auto=compress&cs=tinysrgb&w=600?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Davatar&psig=AOvVaw1Jeb1FK892v0BimW5ZQAcE&ust=1709730021643000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCPCgjuiW3YQDFQAAAAAdAAAAABAE'
+    },
 },
     {
         timestamps:true,
